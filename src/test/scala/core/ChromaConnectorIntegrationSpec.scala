28ca167c084d6f8e6619235ec6c31aab6d764bfe
Lance Paine <lance@lbpaine.com>
2016-06-16 16:43:25 +0100

    Unzip from s3. Add zip files to test resources.

--- src/test/scala/core/ChromaConnectorIntegrationSpec.scala
+++ src/test/scala/core/ChromaConnectorIntegrationSpec.scala
@@ -9,88 +9,53 @@ import awscala.s3.{S3Object, S3ObjectSummary, Bucket, S3}
 import awscala.{BasicCredentialsProvider, Credentials}
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.typesafe.config.ConfigFactory
+import core.ChromaParser.ChromaSingleFlight
 import org.specs2.mutable.SpecificationLike
+import spray.json.{RootJsonFormat, DefaultJsonProtocol}
 import scala.StringBuilder
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.Await
 import scala.concurrent.Future
+import spray.httpx.SprayJsonSupport._
+import scala.concurrent.duration.Duration
+import java.io.{OutputStream, InputStream, File, FileOutputStream}
+import java.util.zip
+import spray.json.DefaultJsonProtocol
+
+import java.nio.charset.StandardCharsets
+import java.util.zip.{ZipEntry, ZipInputStream, ZipOutputStream}
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import awscala.s3.{S3Object, S3ObjectSummary, Bucket, S3}
+import awscala.{BasicCredentialsProvider, Credentials}
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.typesafe.config.ConfigFactory
+import org.specs2.mutable.SpecificationLike
+import spray.json.{RootJsonFormat, DefaultJsonProtocol}
+import scala.StringBuilder
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.Await
+import scala.concurrent.Future
+import spray.httpx.SprayJsonSupport._
 import scala.concurrent.duration.Duration
 import java.io.{OutputStream, InputStream, File, FileOutputStream}
 import java.util.zip
 
-class S3IntegrationSpec extends TestKit(ActorSystem())
-  with SpecificationLike {
-  test =>
+import scala.util.Try
 
-  import awscala._
 
-  implicit val region = Region.EU_WEST_1
-  //  val credentials = BasicCredentialsProvider(S3Secrets.accessKeyId, S3Secrets.s3Secret)
-  implicit val s3 = S3()(region)
-  //  implicit val s3 = S3(credentials)(region)
-  //  val bucket: Bucket = s3.createBucket("unique-name-xxx")
-  //  bucket.put("sample.txt", new java.io.File("sample.txt"))
+object PassengerInfoParser {
 
-  //  import ChromaParser.ChromaParserProtocol._
-  //  import scala.concurrent.ExecutionContext.Implicits.global
-  import system.dispatcher
+  case class PassengerInfo(DocumentIssuingCountryCode: String)
 
-  "We can connect to the S3 bucket" >> {
-    println("starting")
-    val bucketName: String = "drt-deveu-west-1"
-    val result = Bucket(bucketName)
-    val toList: Stream[Either[String, S3ObjectSummary]] = result.ls("")
-    val objects = toList collect {
-      //      case Left(keyName) => println("key:", keyName)
-      case Right(so) =>
-        Future {
-          val obj: Option[S3Object] = result.get(so.getKey)
-          println("obj is ", obj)
-          obj.map {
-            o =>
-              val buffer = new Array[Byte](4096)
-              val zis: ZipInputStream = new ZipInputStream(o.getObjectContent)
-              val nextEntry: ZipEntry = zis.getNextEntry
+  case class FlightPassengerInfoResponse(ArrivalPortCode: String, PassengerList: List[PassengerInfo])
 
-              var ze: ZipEntry = zis.getNextEntry()
-              var lb: List[(String, String, String)] = Nil
-              def getZipEntry(ze: ZipEntry): String = {
-                val sbuilder = new ArrayBuffer[Byte]()
-
-                val fileName = ze.getName()
-                var len: Int = zis.read(buffer)
-
-                while (len > 0) {
-                  sbuilder ++= buffer
-                  len = zis.read(buffer)
-                }
-                val content: String = new String(sbuilder.toArray,StandardCharsets.UTF_8)
-                (content)
-              }
-              while (ze != null) {
-                lb = (so.getKey, nextEntry.getName, getZipEntry(ze)) :: lb
-
-                ze = zis.getNextEntry()
-              }
-
-              zis.closeEntry()
-              zis.close()
-              lb
-          }
-        }
-    }
-
-    val fof: Future[Stream[Option[List[(String, String, String)]]]] = Future.sequence(objects.take(3))
-    Await.ready(fof, Duration.Inf) map {
-      (stream: Stream[Option[List[(String, String, String)]]]) =>
-        stream map {
-          case Some(l) => l.foreach{ file =>
-            println(file._1)
-            println(file._3)
-          }
-        }
-    }
-    false
+  object FlightPassengerInfoProtocol extends DefaultJsonProtocol {
+    implicit val passengerInfoConverter = jsonFormat1(PassengerInfo)
+    implicit val flightPassengerInfoResponseConverter = jsonFormat2(FlightPassengerInfoResponse)
   }
+
 }
--- src/test/scala/core/ChromaParserSpec.scala
+++ src/test/scala/core/ChromaParserSpec.scala
@@ -1,49 +1,75 @@
-//package core
-//
-//import akka.actor.ActorSystem
-//import core.ChromaParser.{ChromaSingleFlight, ChromaToken, ChromaParserProtocol}
-//import http.WithSendAndReceive
-//import org.slf4j.LoggerFactory
-//import org.specs2.mutable.SpecificationLike
-//import spray.client.pipelining._
-//import spray.http.HttpHeaders.{Accept, Authorization}
-//import spray.http.{HttpResponse, HttpRequest, MediaTypes, OAuth2BearerToken}
-//import spray.httpx.SprayJsonSupport._
-//import spray.json.DefaultJsonProtocol
-//
-//import scala.concurrent.{ExecutionContext, Future}
-//
-//object ChromaParser {
-//
-//  case class ChromaToken(access_token: String, token_type: String, expires_in: Int)
-//
-//  case class ChromaSingleFlight(Operator: String,
-//                                Status: String,
-//                                EstDT: String,
-//                                ActDT: String,
-//                                EstChoxDT: String,
-//                                ActChoxDT: String,
-//                                Gate: String,
-//                                Stand: String,
-//                                MaxPax: Int,
-//                                ActPax: Int,
-//                                TranPax: Int,
-//                                RunwayID: String,
-//                                BaggageReclaimId: String,
-//                                FlightID: Int,
-//                                AirportID: String,
-//                                Terminal: String,
-//                                ICAO: String,
-//                                IATA: String,
-//                                Origin: String,
-//                                SchDT: String)
-//
-//  object ChromaParserProtocol extends DefaultJsonProtocol {
-//    implicit val chromaTokenFormat = jsonFormat3(ChromaToken)
-//    implicit val chromaSingleFlightFormat = jsonFormat20(ChromaSingleFlight)
-//  }
-//
-//}
+package core
+
+import java.nio.charset.StandardCharsets
+import java.nio.charset.StandardCharsets._
+import java.util.zip.{ZipEntry, ZipInputStream}
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import awscala.s3.{S3Object, S3ObjectSummary, Bucket, S3}
+import com.amazonaws.services.s3.model.S3ObjectInputStream
+import core.ChromaParser.{ChromaSingleFlight, ChromaToken, ChromaParserProtocol}
+import core.PassengerInfoParser.PassengerInfo
+import http.WithSendAndReceive
+import org.slf4j.LoggerFactory
+import org.specs2.mutable.SpecificationLike
+import spray.client.pipelining._
+import spray.http.HttpHeaders.{Accept, Authorization}
+import spray.http.{HttpResponse, HttpRequest, MediaTypes, OAuth2BearerToken}
+import spray.httpx.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol
+
+import scala.collection.immutable.Iterable
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.{Success, Try}
+object ZipUtils {
+  def getZipEntry(zis: ZipInputStream): String = {
+    val buffer = new Array[Byte](4096)
+    val stringBuffer = new ArrayBuffer[Byte]()
+    var len: Int = zis.read(buffer)
+
+    while (len > 0) {
+      stringBuffer ++= buffer.take(len)
+      len = zis.read(buffer)
+    }
+    val content: String = new String(stringBuffer.toArray, UTF_8)
+    (content)
+  }
+}
+object ChromaParser {
+
+  case class ChromaToken(access_token: String, token_type: String, expires_in: Int)
+
+  case class ChromaSingleFlight(Operator: String,
+                                Status: String,
+                                EstDT: String,
+                                ActDT: String,
+                                EstChoxDT: String,
+                                ActChoxDT: String,
+                                Gate: String,
+                                Stand: String,
+                                MaxPax: Int,
+                                ActPax: Int,
+                                TranPax: Int,
+                                RunwayID: String,
+                                BaggageReclaimId: String,
+                                FlightID: Int,
+                                AirportID: String,
+                                Terminal: String,
+                                ICAO: String,
+                                IATA: String,
+                                Origin: String,
+                                SchDT: String)
+
+  object ChromaParserProtocol extends DefaultJsonProtocol {
+    implicit val chromaTokenFormat = jsonFormat3(ChromaToken)
+    implicit val chromaSingleFlightFormat = jsonFormat20(ChromaSingleFlight)
+  }
+
+}
+
 //trait ChromaParser extends ChromaConfig with WithSendAndReceive {
 ////  self: WithSendAndReceive =>
 //  implicit val system: ActorSystem
@@ -92,63 +118,199 @@
 //  }
 //
 //}
-//
-//class ChromaParserSpec extends
-//  SpecificationLike {
-//
-//  import ChromaParserProtocol._
-//
-//  //  sequential
-//  "Parse chroma response" >> {
-//    "Given a chroma response list with a single Flight we can parse it" in {
-//      import spray.json._
-//      val chromaFlightJson: JsValue =
-//        """
-//          |[{
-//          |"Operator":"Flybe"
-//          	 |,"Status":"On Chocks"
-//          	 |,"EstDT":"2016-06-02T10:55:00Z"
-//          	 |,"ActDT":"2016-06-02T10:55:00Z"
-//          	 |,"EstChoxDT":"2016-06-02T11:01:00Z"
-//          	 |,"ActChoxDT":"2016-06-02T11:05:00Z"
-//          	 |,"Gate":"46"
-//          	 |,"Stand":"44R"
-//          	 |,"MaxPax":78
-//          	 |,"ActPax":51
-//          	 |,"TranPax":0
-//          	 |,"RunwayID":"05L"
-//          	 |,"BaggageReclaimId":"05"
-//          	 |,"FlightID":14710007
-//          	 |,"AirportID":"MAN"
-//          	 |,"Terminal":"T3"
-//          	 |,"ICAO":"BEE1272"
-//          	 |,"IATA":"BE1272"
-//          	 |,"Origin":"AMS"
-//          	 |,"SchDT":"2016-06-02T09:55:00Z"}]
-//        """.stripMargin.parseJson
-//
-//      chromaFlightJson.convertTo[List[ChromaSingleFlight]] should beEqualTo(List(
-//        ChromaSingleFlight("Flybe",
-//          "On Chocks",
-//          "2016-06-02T10:55:00Z",
-//          "2016-06-02T10:55:00Z",
-//          "2016-06-02T11:01:00Z",
-//          "2016-06-02T11:05:00Z",
-//          "46",
-//          "44R",
-//          78,
-//          51,
-//          0,
-//          "05L",
-//          "05",
-//          14710007,
-//          "MAN",
-//          "T3",
-//          "BEE1272",
-//          "BE1272",
-//          "AMS",
-//          "2016-06-02T09:55:00Z")))
-//    }
-//  }
-//  isolated
-//}
+import ChromaParserProtocol._
+import PassengerInfoParser._
+
+class ChromaParserSpec extends
+  SpecificationLike {
+
+
+  //  sequential
+  "Parse chroma response" >> {
+    "Given a chroma response list with a single Flight we can parse it" in {
+      import spray.json._
+      val chromaFlightJson: JsValue =
+        """
+          |[{
+          |"Operator":"Flybe"
+          	 |,"Status":"On Chocks"
+          	 |,"EstDT":"2016-06-02T10:55:00Z"
+          	 |,"ActDT":"2016-06-02T10:55:00Z"
+          	 |,"EstChoxDT":"2016-06-02T11:01:00Z"
+          	 |,"ActChoxDT":"2016-06-02T11:05:00Z"
+          	 |,"Gate":"46"
+          	 |,"Stand":"44R"
+          	 |,"MaxPax":78
+          	 |,"ActPax":51
+          	 |,"TranPax":0
+          	 |,"RunwayID":"05L"
+          	 |,"BaggageReclaimId":"05"
+          	 |,"FlightID":14710007
+          	 |,"AirportID":"MAN"
+          	 |,"Terminal":"T3"
+          	 |,"ICAO":"BEE1272"
+          	 |,"IATA":"BE1272"
+          	 |,"Origin":"AMS"
+          	 |,"SchDT":"2016-06-02T09:55:00Z"}]
+        """.stripMargin.parseJson
+
+      chromaFlightJson.convertTo[List[ChromaSingleFlight]] should beEqualTo(List(
+        ChromaSingleFlight("Flybe",
+          "On Chocks",
+          "2016-06-02T10:55:00Z",
+          "2016-06-02T10:55:00Z",
+          "2016-06-02T11:01:00Z",
+          "2016-06-02T11:05:00Z",
+          "46",
+          "44R",
+          78,
+          51,
+          0,
+          "05L",
+          "05",
+          14710007,
+          "MAN",
+          "T3",
+          "BEE1272",
+          "BE1272",
+          "AMS",
+          "2016-06-02T09:55:00Z")))
+    }
+    //    {
+    //      import spray.json._
+    //      val chromaFlightJson: JsValue =
+    //        """
+    //          |[{
+    //          |"Operator":"Flybe"
+    //          	 |,"Status":"On Chocks"
+    //          	 |,"EstDT":"2016-06-02T10:55:00Z"
+    //          	 |,"ActDT":"2016-06-02T10:55:00Z"
+    //          	 |,"EstChoxDT":"2016-06-02T11:01:00Z"
+    //          	 |,"ActChoxDT":"2016-06-02T11:05:00Z"
+    //          	 |,"Gate":"46"
+    //          	 |,"Stand":"44R"
+    //          	 |,"MaxPax":78
+    //          	 |,"ActPax":51
+    //          	 |,"TranPax":0
+    //          	 |,"RunwayID":"05L"
+    //          	 |,"BaggageReclaimId":"05"
+    //          	 |,"FlightID":14710007
+    //          	 |,"AirportID":"MAN"
+    //          	 |,"Terminal":"T3"
+    //          	 |,"ICAO":"BEE1272"
+    //          	 |,"IATA":"BE1272"
+    //          	 |,"Origin":"AMS"
+    //          	 |,"SchDT":"2016-06-02T09:55:00Z"}]
+    //        """.stripMargin.parseJson
+    //
+    //      chromaFlightJson.convertTo[List[PassengerInfo]] should beEqualTo(List(
+    //        ChromaSingleFlight("Flybe",
+    //          "On Chocks",
+    //          "2016-06-02T10:55:00Z",
+    //          "2016-06-02T10:55:00Z",
+    //          "2016-06-02T11:01:00Z",
+    //          "2016-06-02T11:05:00Z",
+    //          "46",
+    //          "44R",
+    //          78,
+    //          51,
+    //          0,
+    //          "05L",
+    //          "05",
+    //          14710007,
+    //          "MAN",
+    //          "T3",
+    //          "BEE1272",
+    //          "BE1272",
+    //          "AMS",
+    //          "2016-06-02T09:55:00Z")))
+    //    }
+  }
+  isolated
+}
+
+class S3IntegrationSpec extends TestKit(ActorSystem())
+  with SpecificationLike {
+  test =>
+
+  import awscala._
+
+  implicit val region = Region.EU_WEST_1
+  //  val credentials = BasicCredentialsProvider(S3Secrets.accessKeyId, S3Secrets.s3Secret)
+  implicit val s3 = S3()(region)
+  //  implicit val s3 = S3(credentials)(region)
+  //  val bucket: Bucket = s3.createBucket("unique-name-xxx")
+  //  bucket.put("sample.txt", new java.io.File("sample.txt"))
+
+  //  import ChromaParser.ChromaParserProtocol._
+  //  import scala.concurrent.ExecutionContext.Implicits.global
+  import system.dispatcher
+
+  "We can connect to the S3 bucket" >> {
+    println("starting")
+    val bucketName: String = "drt-deveu-west-1"
+    val result = Bucket(bucketName)
+    val toList: Stream[Either[String, S3ObjectSummary]] = result.ls("")
+    val objects = toList collect {
+      //      case Left(keyName) => println("key:", keyName)
+      case Right(so) =>
+        Future {
+          val obj: Option[S3Object] = result.get(so.getKey)
+          //          println("obj is ", obj)
+          obj.map(_.getObjectContent).map {
+            zippedFileStream =>
+              val unzippedStream: ZipInputStream = new ZipInputStream(zippedFileStream)
+
+              var ze: ZipEntry = unzippedStream.getNextEntry()
+              var lb: List[(String, String, String)] = Nil
+
+              while (ze != null) {
+                val name: String = ze.getName
+                val entry: String = ZipUtils.getZipEntry(unzippedStream)
+                lb = (so.getKey, name, entry) :: lb
+                ze = unzippedStream.getNextEntry()
+              }
+
+              unzippedStream.closeEntry()
+              unzippedStream.close()
+              lb
+          }
+        }
+    }
+
+
+
+    import spray.json._
+    import PassengerInfoParser._
+    import FlightPassengerInfoProtocol._
+
+    val res: Stream[Future[List[(String, Try[Iterable[(String, List[PassengerInfo], Int)]])]]] = objects.map { future =>
+      val futureMapped = future collect {
+        case Some(l) => l map { file =>
+          println("=" * 80)
+          val zipFileName: String = file._1
+          println(zipFileName)
+          val zipEntryFileName: String = file._2
+          println(zipEntryFileName)
+          val parsed: JsValue = file._3.parseJson
+          val triedPassengerInfoResponse: Try[FlightPassengerInfoResponse] = Try(parsed.convertTo[FlightPassengerInfoResponse])
+          (zipFileName + ":" + zipEntryFileName -> triedPassengerInfoResponse.map { passengerInfoResponse =>
+            val passengerListByCountryCode = passengerInfoResponse.PassengerList.groupBy(_.DocumentIssuingCountryCode)
+            passengerListByCountryCode.map(pair => (pair._1, pair._2, pair._2.length))
+          })
+        }
+      }
+      futureMapped
+    }
+    println(Await.ready(Future.sequence(res.take(5)), Duration.Inf).value.mkString("\n"))
+    false
+  }
+
+
+  val passengerInfo =
+    """
+
+    """.stripMargin
+}
+
+
